b0549e408d0617eaf768c97a842918b2bd731bad
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-01-07 20:20:06 -1000

    Q5

--- exercises.js
+++ exercises.js
@@ -87,7 +87,7 @@ console.log(difference);
 */
 
 function multiply(num1 , num2){
-	return num1*num2
+	return num1*num2;
 }
 var product = multiply(bango1 , bango2);
 console.log(product);
@@ -106,7 +106,11 @@ console.log(product);
  * Console.log `quotient` to test your code.
 */
 
-
+function divide(num1 , num2){
+	return num1/num2
+}
+var quotient = divide(bango1 , bango2);
+console.log(quotient);
 
 /*
  * #6
